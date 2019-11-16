3b98d0341d8c7fac0303d8677af518520a9605d6
Nishchal Gautam <gautam.nishchal@gmail.com>
2019-11-16 19:39:40 +0700

    test(integration): fix postman tests

--- Micro.KeyStore.UnitTest/ExternalTests/postman_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-echo '{"values": [{"key": "url","value": "http://localhost:5000"}]}' > env.json
-npx newman run https://www.getpostman.com/collections/08bfa7cf05323e4194fd -e ./env.json && rm env.json
